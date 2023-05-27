03f68e2b07741c9bb8589e652d64adf3b3b7aa73
DarkhanShakhan <darkhan.shakhanov@gmail.com>
2023-05-28 03:22:12 +0600

    Best-option (#5)
    * adds new option choice
    
    * adds bitwise functions
    
    * refactor the layout

--- src/bot/anfield.rs
+++ src/bot/anfield.rs
@@ -1,14 +1,17 @@
-pub struct Piece {
-    piece: Vec<Vec<char>>,
+use crate::util::Validate;
+
+pub struct Anfield {
+    board: Vec<Vec<char>>,
     size: (i32, i32),
     is_collect: bool,
     counter: i32,
 }
 
-impl Piece {
+//TODO: make board stable; without collecting it all the time; update it instead
+impl Anfield {
     pub fn new() -> Self {
-        Piece {
-            piece: vec![],
+        Anfield {
+            board: vec![],
             size: (0, 0),
             is_collect: false,
             counter: 0,
@@ -24,27 +27,31 @@ impl Piece {
         self.counter = 0;
     }
 
-    pub fn is_empty(&self) -> bool {
-        self.piece.len() == 0
-    }
-
     pub fn set_size(&mut self, size: (i32, i32)) {
         self.size = size
     }
 
-    pub fn collect(&mut self, input: String) {
-        self.piece.push(input.chars().collect());
+    pub fn collect(&mut self, input: &str) {
         self.counter += 1;
-        if self.counter >= self.size.1 {
+        if self.counter > 1 {
+            self.board.push(input.chars().collect());
+        }
+        if self.counter > self.size.1 {
             self.is_collect = false;
         }
     }
 
     pub fn clear(&mut self) {
-        self.piece.clear();
+        self.board.clear();
     }
 
-    pub fn get_piece(&mut self) -> &Vec<Vec<char>> {
-        self.piece.as_mut()
+    pub fn get_board(&mut self) -> &Vec<Vec<char>> {
+        self.board.as_mut()
+    }
+}
+
+impl Validate for Anfield {
+    fn len(&self) -> usize {
+        self.board.len()
     }
 }
